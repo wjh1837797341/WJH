d7d1ad0080c0066de179262a96b601bdaca20f52
wjh <1837797341@qq.com>
2021-07-08 16:46:12 +0800

    first commit

---
+++ src/main/java/com/example/wjh/WjhApplication.java
@@ -0,0 +1,13 @@
+package com.example.wjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WjhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WjhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wjh/WjhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wjh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WjhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
